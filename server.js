63a9493ef3f7d0dfc5211a7f693fba31ca40358f
kharman82 <kharman82@gmail.com>
2020-04-21 12:45:12 -0700

    server up and running

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 'use strict'  
+
 require('dotenv').config();
 const express = require('express');
-const app = express;
+const app = express();
 const superagent = require('superagent');
 const pg = require('pg');
 const PORT = process.env.PORT || 3001;
 const ejs = require('ejs');
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({extended: true}));
 // set the view engine for the server
+
 app.set('view engine', 'ejs');
 // static route
 app.use(express.static('public'));
 
-app.get('/hello', (request, response) =>{
+app.get('/hello', (request, response) => {
     response.send('hello from ejs')
 });
 
